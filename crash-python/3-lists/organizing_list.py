8457dbed3d8448eb8b8c9710c6c3bfde9495cd8e
Allan19Prieto <allanbanilla@gmail.com>
2026-04-21 23:30:02 -0600

    Examples about list

---
+++ crash-python/3-lists/organizing_list.py
@@ -0,0 +1,13 @@
+# sorting a list permanently with the sort() method
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.sort()
+print(cars)
+
+#printing a list in reverse order
+print(cars)
+cars.reverse()
+print(cars)
+
+# finding the length of a list
+print(len(cars))
