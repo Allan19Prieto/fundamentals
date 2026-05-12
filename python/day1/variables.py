aaaa191b26704f9b5ff3d866ed35395d2dc6ca81
Allan19Prieto <allanbanilla@gmail.com>
2026-05-12 11:04:06 -0600

    gay 1 learning python: variables in python

---
+++ python/day1/variables.py
@@ -0,0 +1,9 @@
+# how to use the print function
+print("Hello, World!")
+
+# hot to get user input
+name = input("What is your name? ")
+
+# How tu count the number of characters in a string
+name_length = len(name)
+
