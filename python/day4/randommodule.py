484ba723d1a25abea5cf5610f535b853bed6b5de
Allan19Prieto <allanbanilla@gmail.com>
2026-06-11 22:49:10 -0600

    list in day 4

--- python/day4/randommodule.py
+++ python/day4/randommodule.py
@@ -16,3 +16,10 @@ print("####################################")
 random_float = random.uniform(1, 10)
 print(random_float)
 
+random_heads_or_tails = random.randint(0, 1)
+if random_heads_or_tails == 0:
+    print("Heads")
+else:
+    print("Tails")
+
+
