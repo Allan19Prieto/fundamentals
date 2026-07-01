0efe54878a16cf57b818e355e1cf8e8c0eea2441
Allan19Prieto <allanbanilla@gmail.com>
2026-06-30 22:52:11 -0600

    Day 4 end

--- python/day4/rockpaperscissots.py
+++ python/day4/rockpaperscissots.py
@@ -34,11 +34,16 @@ computer_choice = random.randint(0,2)
 print("Computer chose:")
 print(game_images[computer_choice])
 
-if user_choice == 2 and computer_choice == 0:
+if user_choice >= 3 or user_choice < 0:
+     print("You typed an invalid number, you lose!")
+
+elif user_choice == 2 and computer_choice == 0:
     print("You lose")
 elif computer_choice > user_choice:
     print("You lose")
 elif user_choice > computer_choice:
     print("You win")
+elif user_choice == computer_choice:
+      print("It's a draw")
 
 
