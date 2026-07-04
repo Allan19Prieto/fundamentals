88a45f864431a0e52b7db45b0a12e2cbb2c427f3
Allan19Prieto <allanbanilla@gmail.com>
2026-07-03 22:51:37 -0600

    Day 1 of python book

---
+++ fundamentals_python/week1/for/gauss.py
@@ -0,0 +1,23 @@
+# add all the numbers from 1 to 100 using a for loop
+
+total = 0
+for i in range(101):
+    total += i
+print(total)
+
+print("")
+# starting, stopping
+for i in range(12, 16):
+    print(i)
+
+print("")
+# starting, stopping, step
+for i in range(0, 10, 2):
+    print(i)
+
+print("")
+# negative step
+for i in range(5, -1, -1):
+    print(i)
+
+
