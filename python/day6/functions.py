bb637f5004f036603b3a0a787d5f5d6d72018fef
Allan19Prieto <allanbanilla@gmail.com>
2026-07-08 22:50:43 -0600

    Day 6 functions

---
+++ python/day6/functions.py
@@ -0,0 +1,13 @@
+# print is a function
+print("Hello Allan")
+num_char = len("Hello")
+print(num_char)
+
+# how to define a function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+
